aa8a6193d092ceac298ede9616aadc17283ba155
jordanzet <jordanzet@gmail.com>
2014-03-23 13:27:43 -0500

    mejoras para el deployment

---
+++ comunidad/local_settings.py
@@ -0,0 +1,10 @@
+DATABASES = { 
+	'default': { 
+		'ENGINE': 'django.db.backends.mysql', 
+		'NAME': 'bdcomunidad', 
+		'USER': 'root', 
+		'PASSWORD': '123456', 
+		'HOST': 'localhost', 
+		'PORT': '' 
+	}
+}
